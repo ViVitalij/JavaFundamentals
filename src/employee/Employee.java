1766a3aec478256c5fb8ddd2d613d01ee98df3c0
ViVitalij <kontrabasista@wp.pl>
2017-02-17 17:22:35 +0100

    Employee: add unit test, static NextId and non-static id fields. EmployeeTest: add proper call a static method.

--- src/employee/Employee.java
+++ src/employee/Employee.java
@@ -5,15 +5,20 @@ import java.time.LocalDate;
 /**
  * Created by m.losK on 2017-02-17.
  */
-public class Employee {
+class Employee {
+
+    private static int nextId = 1;
+
     private String name;
     private double salary;
     private LocalDate hireDay;
+    private int id;
 
     public Employee(String name, double salary, int day, int month, int year) {
         this.name = name;
         this.salary = salary;
         this.hireDay = LocalDate.of(year, month, day);
+        this.id=0;
     }
 
     public String getName() {
@@ -28,15 +33,36 @@ public class Employee {
         return hireDay;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId() {
+        this.id = nextId;
+        nextId++;
+    }
+
+    public static int getNextId() {
+        return nextId;
+    }
+
     public void raiseSalary(double byPercent){
-        double raise = salary * byPercent/100;
-        salary += raise;
+        double raise = this.salary * byPercent/100;
+        this.salary += raise;
     }
 
     @Override
     public String toString() {
-        return  name +
-                ", salary: " + salary +
-                ", hireDay: " + hireDay;
+        return  "Id " + this.id + " " + this.name +
+                ", salary: " + this.salary +
+                ", hireDay: " + this.hireDay;
+    }
+
+    /**
+     *  unit test
+     */
+    public static void main(String[] args) {
+        Employee e = new Employee("Mateusz", 10000, 10,10,1986);
+        System.out.println(e.getName() + " " + e.getSalary());
     }
 }
--- src/employee/EmployeeTest.java
+++ src/employee/EmployeeTest.java
@@ -8,16 +8,20 @@ public class EmployeeTest {
     public static void main(String[] args) {
         Employee[] staff = new Employee[4];
 
-        staff[0] = new Employee("Mateusz Loska", 10000, 17,2,2016);
-        staff[1] = new Employee("Anna Loska", 8000, 1,3,2016);
-        staff[2] = new Employee("Piotr Cienkowski", 4000, 12,6,2016);
+        staff[0] = new Employee("Mateusz Loska", 10000, 17, 2, 2016);
+        staff[1] = new Employee("Anna Loska", 8000, 1, 3, 2016);
+        staff[2] = new Employee("Piotr Cienkowski", 4000, 12, 6, 2016);
         staff[3] = new Employee("Pawel Stepniak", 4500, 12, 9, 2016);
 
         for (Employee e : staff) {
             e.raiseSalary(5);
         }
         for (Employee e : staff) {
-            System.out.println("After salary raise: "+e.toString());
+            e.setId();
+            System.out.println("After salary raise: " + e.toString());
         }
+
+        int nextAvailableId = Employee.getNextId();     //proper call a static method (instead of e.g. staff[0].getNextId();
+        System.out.println("Next available identifier is " + nextAvailableId);
     }
 }
